143b283cf1b461498c496dd67c0444a8ecaa8445
kahonozue <smilekaho@gmail.com>
2022-12-13 10:58:07 +0100

    weekplanner / navbar(weekplanner)

--- src/Form/WeekplannerType.php
+++ src/Form/WeekplannerType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Weekplanner;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +13,24 @@ class WeekplannerType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('date')
-            ->add('mealTime')
+            // ->add('date')
+            ->add('weekday', ChoiceType::class, [
+                'choices' => [
+                    'Mon.' => "Mon",
+                    'Tue.' => "Tue.",
+                    'Wed.' => "Wed.",
+                    'Thu.' => "Thu.",
+                    'Fri.' => "Fri.",
+                    'Sat.' => "Sat.",
+                    'Sun.' => "Sun."],
+                "attr"=>["class"=>"form-control mb-2"]])
+                
+            ->add('mealTime', ChoiceType::class, [
+                'choices' => [
+                    'Breakfast' => "Breakfast",
+                    'Lunch' => "Lunch",
+                    'Dinner' => "Dinner"],
+                "attr"=>["class"=>"form-control mb-2"]])
         ;
     }
 
